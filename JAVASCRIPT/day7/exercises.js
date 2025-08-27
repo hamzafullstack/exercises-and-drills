7f7ec63c9a29f8063e82fe94eb77e7c070df71b5
Ameer Hamza Bugti <ameerhamza.fullstack@gmail.com>
2025-08-27 08:12:00 +0400

    ADD  day7 JS Type Conversion Exercises
    exercise rules and instructions and the solutions. and some flash cards related to the  JS type conversion.

---
+++ JAVASCRIPT/day7/exercises.js
@@ -0,0 +1,16 @@
+// exercise solutions..
+let userInput = '42'; // 42 is string
+num = Number(userInput); // convert str input to number
+alert(num + 8); // 50
+
+let emptyInput = ''; // empty input
+bol = Boolean(emptyInput); // converted emptyinput in boolean
+alert(bol); // false.
+
+let randomValue = null; // random value input.
+alert(Number(randomValue)); // 0
+
+let numAndStr = "123abc"; // its a var value is string for now
+numOnly = Number(numAndStr); // converting str to num using Number function
+alert(numOnly); // NaN not a number because that input caries alphabets.
+
